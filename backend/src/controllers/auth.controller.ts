0f8e0e06f93c0f9bfbae952b7373e3fb5683f8cb
amirrajj-dev <amiramraja@gmail.com>
2025-02-21 15:27:30 +0330

    added some necessary auth routes🎓💚

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+
+export const signup = (req : Request , res : Response)=>{}
+
+export const signin = (req : Request , res : Response)=>{}
+
+export const signout = (req : Request , res : Response)=>{}
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+import { signin, signout, signup } from '../controllers/auth.controller';
 
 const router = express.Router()
+
+router.use('/signup' , signup)
+router.use('/sigin' , signin)
+router.use('/signout' , signout)
 
 export default router;
