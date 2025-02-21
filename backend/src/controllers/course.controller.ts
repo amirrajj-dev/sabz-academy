3e2ed6b943ed394f0da0ba05d8cb6f6eb5e4eeea
amirrajj-dev <amiramraja@gmail.com>
2025-02-21 19:42:44 +0330

    add some necessary course routes and correspond their correspond controller🎓💚

---
+++ backend/src/controllers/course.controller.ts
@@ -0,0 +1,10 @@
+import type {Response , Request , NextFunction} from 'express'
+
+
+export const getAllCourses = async (req : Request, res : Response , next : NextFunction) =>{}
+export const createCourse = async (req : Request, res : Response , next : NextFunction) =>{}
+export const deleteCourse = async (req : Request, res : Response , next : NextFunction) =>{}
+export const updateCourse = async (req : Request, res : Response , next : NextFunction) =>{}
+export const getSingleCourse = async (req : Request, res : Response , next : NextFunction) =>{}
+export const createSession = async (req : Request, res : Response , next : NextFunction) =>{}
+export const getAllSessions = async (req : Request, res : Response , next : NextFunction) =>{}
--- backend/src/routes/course.route.ts
+++ backend/src/routes/course.route.ts
@@ -1,5 +1,19 @@
 import express from 'express'
-
+import {getAllCourses,
+    createCourse,
+    deleteCourse,
+    updateCourse,
+    getSingleCourse,
+    createSession,
+    getAllSessions} from '../controllers/course.controller'
 const router = express.Router()
+
+router.get('/' , getAllCourses)
+router.post('/' , createCourse)
+router.delete('/:id' , deleteCourse)
+router.put('/:id' , updateCourse)
+router.get('/:id' , getSingleCourse)
+router.post('/:id/sessions' , createSession)
+router.get('/:id/sessions' , getAllSessions)
 
 export default router;
