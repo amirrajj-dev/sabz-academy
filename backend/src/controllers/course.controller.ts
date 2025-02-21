e9b90059d2d757e1c2809aa27bd1be87cbef5012
amirrajj-dev <amiramraja@gmail.com>
2025-02-21 19:50:28 +0330

    get all curses route completed🎓💚

--- backend/src/controllers/course.controller.ts
+++ backend/src/controllers/course.controller.ts
@@ -1,7 +1,17 @@
 import type {Response , Request , NextFunction} from 'express'
+import prisma from '../../utils/prisma'
 
-
-export const getAllCourses = async (req : Request, res : Response , next : NextFunction) =>{}
+export const getAllCourses = async (req : Request, res : Response , next : NextFunction) =>{
+    try {
+        const courses = await prisma.course.findMany()
+        return res.status(200).json({
+            success: true,
+            data: courses,
+        })
+    } catch (error) {
+     next(error)       
+    }
+}
 export const createCourse = async (req : Request, res : Response , next : NextFunction) =>{}
 export const deleteCourse = async (req : Request, res : Response , next : NextFunction) =>{}
 export const updateCourse = async (req : Request, res : Response , next : NextFunction) =>{}
