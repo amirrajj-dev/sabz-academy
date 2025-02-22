1db85b3e8175a6224b20e37132c3cb63bb04e4c4
amirrajj-dev <amiramraja@gmail.com>
2025-02-22 15:42:30 +0330

    get all categories logic and functioanallity completed🎓💚

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -1,7 +1,17 @@
 import type { NextFunction, Request, Response } from "express";
 import prisma from "../../utils/prisma";
 
-export const getAllCategories = async (req : Request , res : Response , next : NextFunction)=>{}
+export const getAllCategories = async (req : Request , res : Response , next : NextFunction)=>{
+    try {
+        const categories = await prisma.category.findMany()
+        return res.status(200).json({
+            success: true,
+            data: categories,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
 export const createCategory = async (req : Request , res : Response , next : NextFunction)=>{}
 export const deleteCategory = async (req : Request , res : Response , next : NextFunction)=>{}
 export const updateCategory = async (req : Request , res : Response , next : NextFunction)=>{}
