1896e585867ea3801e5cf48f8bf5b8fcfc361b23
amirrajj-dev <amiramraja@gmail.com>
2025-02-22 15:06:48 +0330

    added necessary menu routes and controllers🎓💚

---
+++ backend/src/controllers/menu.controller.ts
@@ -0,0 +1,6 @@
+import type { NextFunction, Request, Response } from "express";
+import prisma from "../../utils/prisma";
+
+export const createMenu = async (req : Request , res : Response , next : NextFunction)=>{}
+export const getAllMenues = async (req : Request , res : Response , next : NextFunction)=>{}
+export const deleteMenues = async (req : Request , res : Response , next : NextFunction)=>{}
--- backend/src/routes/menu.route.ts
+++ backend/src/routes/menu.route.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import { protectRouteAdmin } from '../middlewares/authMiddleware';
+import { createMenu , deleteMenues , getAllMenues } from '../controllers/menu.controller';
 
 const router = express.Router()
+
+router.post('/' , protectRouteAdmin as express.RequestHandler , createMenu as express.RequestHandler)
+router.get('/' , getAllMenues as express.RequestHandler)
+router.delete('/:id' , deleteMenues as express.RequestHandler)
 
 export default router;
