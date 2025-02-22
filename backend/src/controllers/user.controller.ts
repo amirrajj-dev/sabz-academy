48cb617a83604faa64bde1f3bab5f39e0fe8b699
amirrajj-dev <amiramraja@gmail.com>
2025-02-22 14:33:30 +0330

    delete user logic and functiuonallity completed🎓💚

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -28,9 +28,13 @@ export const getAllUsers = async (
   }
 };
 
-export const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const { username, email, password, phone , name } = req.body;
+    const { username, email, password, phone, name } = req.body;
 
     if (!username && !email && !password && !phone && !name) {
       return res.status(400).json({
@@ -78,7 +82,7 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
       select: {
         id: true,
         username: true,
-        name : true,
+        name: true,
         email: true,
         phone: true,
         role: true,
@@ -92,7 +96,6 @@ export const updateUser = async (req: Request, res: Response, next: NextFunction
       success: true,
       data: updatedUser,
     });
-
   } catch (error) {
     console.error("Error updating user:", error);
     next(error);
@@ -102,7 +105,43 @@ export const deleteUser = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  try {
+    const userId = req.params.id;
+    if (!userId) {
+      return res.status(400).json({
+        message: "userId is required",
+        success: false,
+      });
+    }
+    const user = await prisma.user.findUnique({
+      where : {id : userId}
+    })
+    if (!user) {
+      return res.status(404).json({
+        message : 'user not found',
+        success : false
+      })
+    }
+    if (user.role === 'ADMIN'){
+      return res.status(400).json({
+        message : 'Admins cannot be deleted',
+        success : false
+      })
+    }
+    await prisma.user.delete({
+      where : {
+        id : user.id
+      }
+    })
+    return res.status(200).json({
+      message : "User deleted successfully",
+      success : true
+    })
+  } catch (error) {
+    next(error);
+  }
+};
 export const banUser = async (
   req: Request,
   res: Response,
