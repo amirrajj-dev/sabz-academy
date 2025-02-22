46b67999254805be132d42dc8396505c551690a9
amirrajj-dev <amiramraja@gmail.com>
2025-02-22 15:39:24 +0330

    add category necessary routes and controllers🎓💚

---
+++ backend/src/controllers/category.controller.ts
@@ -0,0 +1,7 @@
+import type { NextFunction, Request, Response } from "express";
+import prisma from "../../utils/prisma";
+
+export const getAllCategories = async (req : Request , res : Response , next : NextFunction)=>{}
+export const createCategory = async (req : Request , res : Response , next : NextFunction)=>{}
+export const deleteCategory = async (req : Request , res : Response , next : NextFunction)=>{}
+export const updateCategory = async (req : Request , res : Response , next : NextFunction)=>{}
--- backend/src/routes/category.route.ts
+++ backend/src/routes/category.route.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import { protectRouteAdmin } from '../middlewares/authMiddleware'
+import { getAllCategories , createCategory , deleteCategory , updateCategory } from '../controllers/category.controller'
 
 const router = express.Router()
 
+router.get('/' , getAllCategories as express.RequestHandler)
+router.post('/' , protectRouteAdmin as express.RequestHandler , createCategory as express.RequestHandler)
+router.delete('/:id' , deleteCategory as express.RequestHandler)
+router.put('/' , protectRouteAdmin as express.RequestHandler , updateCategory as express.RequestHandler)
 export default router;
