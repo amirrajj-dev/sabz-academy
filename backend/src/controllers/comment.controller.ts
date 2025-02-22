1f04e09fa57118f8a8254857074a1d732bc5f73a
amirrajj-dev <amiramraja@gmail.com>
2025-02-22 18:21:27 +0330

    get all comments logic and functionallty completed🎓💚

--- backend/src/controllers/comment.controller.ts
+++ backend/src/controllers/comment.controller.ts
@@ -1,7 +1,34 @@
 import type { NextFunction, Request, Response } from "express";
 import prisma from "../../utils/prisma";
 
-export const getAllComments = async (req : Request , res : Response , next : NextFunction)=>{}
+export const getAllComments = async (req : Request , res : Response , next : NextFunction)=>{
+    try {
+        const user = req.user
+        if (!user){
+            return res.status(401).json({ message: "User not authenticated", success: false });
+        }
+        const comments = await prisma.comment.findMany({
+            include : {
+                creator : {
+                    select : {
+                        id : true,
+                        email : true,
+                        username : true,
+                    }
+                },
+                course : {
+                    select : {
+                        id : true,
+                        name : true,
+                    }
+                }
+            }
+        })
+        return res.status(200).json({ message: "Comments fetched successfully", success: true, comments })
+    } catch (error) {
+        next(error)
+    }
+}
 export const createComment = async (req : Request , res : Response , next : NextFunction)=>{}
 export const deleteComment = async (req : Request , res : Response , next : NextFunction)=>{}
 export const answerComment = async (req : Request , res : Response , next : NextFunction)=>{}
