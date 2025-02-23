bb5c689dd3bab8a56783cafb550dfd69bcf919c0
amirrajj-dev <amiramraja@gmail.com>
2025-02-24 01:53:02 +0330

    added framer motion and a little change in  eslint configs🎓💚

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  
+  images : {remotePatterns : [
+    {
+        hostname : "*"
+    }
+  ]}
 };
 
 export default nextConfig;
