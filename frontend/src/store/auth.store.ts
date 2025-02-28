f831e74524d82a977279d76920d9aab4f4bb4625
amirrajj-dev <amiramraja@gmail.com>
2025-02-28 15:01:53 +0330

    completed login functionallity🎓💚

--- frontend/src/store/auth.store.ts
+++ frontend/src/store/auth.store.ts
@@ -7,7 +7,7 @@ const emailReg = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
 interface AuthStore {
     user: IUser | null;
     signup : (user : Pick<IUser , 'name' | 'username' | 'phone' | 'email' | 'password'>) => Promise<{error? : string , success : boolean, message? : string}>
-    login: (user : Pick<IUser , 'email' | 'password'>) => void;
+    login: (user : Pick<IUser , 'email' | 'password'>) => Promise<{message : string , success : boolean}>;
     logout: () => void;
     setUser: (user : IUser | null) => void;
     isAuthenticated : boolean
@@ -52,7 +52,44 @@ export const useAuthStore = create<AuthStore>((set) => ({
             set({ isLoading: false });
         }
     },
-    login: (user) => {},
+    login: async (user) => {
+        try {
+            set({isLoading : true})
+            const {email , password} = user
+            console.log(email , password);
+            if (!email.trim() ||!password.trim()) {
+                throw new Error("All fields are required");
+            }
+            if (password.length < 6){
+                throw new Error("Password must be at least 6 characters long");
+            }
+            if (!emailReg.test(email)){
+                throw new Error("Invalid email");
+            }
+            const res = await axiosnInstance.post('/auth/signin', user)
+            if (res.data.success){
+                set({ user: res.data.user, isAuthenticated: true, error: null, isLoading: false });
+                return {
+                    success : res.data.success,
+                    message : res.data.message
+                }
+            }else{
+                set({ error: res.data.message });
+                return {
+                    success : false,
+                    message : res.data.message
+                }
+            }
+        } catch (error : any) {
+            set({ error: error.response.data.message || error.message });
+            return {
+                success : false,
+                message : error.response.data.message || error.message
+            }
+        }finally{
+            set({isLoading : false});
+        }
+    },
     logout: async () => {
         try {
             set({ isLoading: true });
@@ -69,7 +106,9 @@ export const useAuthStore = create<AuthStore>((set) => ({
             set({ isLoading: false });
         }
     },
-    setUser: (user) => {},
+    setUser: (user) => {
+        set({ user });
+    },
     getMe: async () => {
         try {
             set({isLoading : true})
