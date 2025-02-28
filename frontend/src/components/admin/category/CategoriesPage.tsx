8908072e852443cc4aa956e2b01c4dcdec0fb970
amirrajj-dev <amiramraja@gmail.com>
2025-02-28 16:42:00 +0330

    added skeleton for categories table🎓💚

--- frontend/src/components/admin/category/CategoriesPage.tsx
+++ frontend/src/components/admin/category/CategoriesPage.tsx
@@ -1,29 +1,27 @@
 'use client'
 import React, { useEffect } from "react";
 import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
-import AddCategoryModal from "../..//modals/category/AddCategoryModal";
+import AddCategoryModal from "../../modals/category/AddCategoryModal";
 import EditCategoryModal from "../../modals/category/EditCategoryModal";
 import DeleteModal from "@/components/modals/shared/DeleteModal";
 import { useCategoriesStore } from "@/store/category.store";
 
 const CategoriesPage = () => {
+  const { isLoading, fetchCategories, categories } = useCategoriesStore();
 
-  const {isLoading , fetchCategories , categories} = useCategoriesStore()
   useEffect(() => {
-    fetchCategories()
-  }, [])
+    fetchCategories();
+  }, []);
 
   return (
     <div className="p-6 max-w-5xl mx-auto">
-
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-0 justify-between items-center mb-6">
         <h1 className="text-3xl font-extrabold text-base-content">📂 دسته‌بندی‌ها</h1>
-       <AddCategoryModal/>
+        <AddCategoryModal />
       </div>
 
       <div className="shadow-lg rounded-xl overflow-x-auto">
         <table className="table bg-base-300 min-w-[600px] w-full text-center">
-
           <thead className="bg-base-300 text-base-content">
             <tr>
               <th className="p-4 whitespace-nowrap">شناسه</th>
@@ -35,28 +33,59 @@ const CategoriesPage = () => {
           </thead>
 
           <tbody>
-            {categories?.map((category, index) => (
-              <tr
-                key={category.id}
-                className={`border-b hover:bg-base-300 transition ${
-                  index % 2 === 0 ? "bg-base-200" : "bg-base-100"
-                }`}
-              >
-                <td className="p-4 font-medium text-base-content">{index + 1}</td>
-                <td className="p-4 font-semibold text-base-content">{category.name}</td>
-                <td className="p-4">
-                  <span className="text-primary hover:underline">
-                    {category.title}
-                  </span>
-                </td>
-                <td className="p-4">
-                 <EditCategoryModal />
-                </td>
-                <td className="p-4">
-                  <DeleteModal title="حذف دسته بندی 📂" message="آیا از حذف این دسته بندی اطمینان دارید" deleteBtnText="حذف دسته بندی" messageDesc="این اقدام قابل بازگشت نیست !"  />
+            {isLoading ? (
+              [...Array(4)].map((_, index) => (
+                <tr key={index} className="border-b bg-base-200">
+                  <td className="p-4">
+                    <div className="skeleton h-5 w-16"></div>
+                  </td>
+                  <td className="p-4">
+                    <div className="skeleton h-5 w-32"></div>
+                  </td>
+                  <td className="p-4">
+                    <div className="skeleton h-5 w-40"></div>
+                  </td>
+                  <td className="p-4">
+                    <div className="skeleton btn rounded-full"></div>
+                  </td>
+                  <td className="p-4">
+                    <div className="skeleton btn rounded-full"></div>
+                  </td>
+                </tr>
+              ))
+            ) : categories?.length > 0 ? (
+              categories.map((category, index) => (
+                <tr
+                  key={category.id}
+                  className={`border-b hover:bg-base-300 transition ${
+                    index % 2 === 0 ? "bg-base-200" : "bg-base-100"
+                  }`}
+                >
+                  <td className="p-4 font-medium text-base-content">{index + 1}</td>
+                  <td className="p-4 font-semibold text-base-content">{category.name}</td>
+                  <td className="p-4">
+                    <span className="text-primary hover:underline">{category.title}</span>
+                  </td>
+                  <td className="p-4">
+                    <EditCategoryModal />
+                  </td>
+                  <td className="p-4">
+                    <DeleteModal
+                      title="حذف دسته بندی 📂"
+                      message="آیا از حذف این دسته بندی اطمینان دارید"
+                      deleteBtnText="حذف دسته بندی"
+                      messageDesc="این اقدام قابل بازگشت نیست !"
+                    />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={5} className="p-6 text-lg text-base-content">
+                  هیچ دسته‌بندی‌ای وجود ندارد 😔
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
