8d0447e9162dd9234ca7c5bc73ef9ad74fb33844
amirrajj-dev <amiramraja@gmail.com>
2025-02-28 20:29:48 +0330

    add skeleton for users table🎓💚

--- frontend/src/components/admin/user/UsersPage.tsx
+++ frontend/src/components/admin/user/UsersPage.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import { FaEdit, FaTrash, FaPlus, FaBan, FaUserShield } from "react-icons/fa";
 import { motion } from "framer-motion";
@@ -10,34 +10,37 @@ import { toast } from "react-toastify";
 import { toastOptions } from "@/helpers/toastOptions";
 
 const UsersPage = () => {
-
-  const {users , fetchUsers , deleteUser , banUser} = useUserStore()
+  const { users, fetchUsers, isLoading, deleteUser, banUser } = useUserStore();
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
   const handleDelete = async (id: string) => {
-    const res = await deleteUser(id)
+    const res = await deleteUser(id);
     if (res.success) {
-      toast.success('کاربر با موفقیت حذف شد' , toastOptions)
-    }else{
-      toast.error('خطا در حذف کاربر' , toastOptions)
+      toast.success("کاربر با موفقیت حذف شد", toastOptions);
+    } else {
+      toast.error("خطا در حذف کاربر", toastOptions);
     }
   };
 
   const handleBan = async (id: string) => {
-    const res = await banUser(id)
+    const res = await banUser(id);
     console.log(res);
     if (res.success) {
-      res.message === "User unbanned successfully" ? toast.success('کاربر با موفقیت از بن خارج شد' , toastOptions) : toast.success('کاربر با موفقیت بن شد' , toastOptions)
-    }else{
-      toast.error('خطا در بن کاربر' , toastOptions)
+      res.message === "User unbanned successfully"
+        ? toast.success("کاربر با موفقیت از بن خارج شد", toastOptions)
+        : toast.success("کاربر با موفقیت بن شد", toastOptions);
+    } else {
+      toast.error("خطا در بن کاربر", toastOptions);
     }
-  }
+  };
 
   return (
     <div className="p-6 max-w-5xl mx-auto">
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-0 justify-between items-center mb-6">
-        <h1 className="text-3xl font-extrabold text-base-content">👥 کاربران</h1>
+        <h1 className="text-3xl font-extrabold text-base-content">
+          👥 کاربران
+        </h1>
       </div>
 
       <div className="shadow-lg rounded-xl overflow-x-auto">
@@ -56,29 +59,80 @@ const UsersPage = () => {
           </thead>
 
           <tbody>
-            {users.map((user, index) => (
-              <tr
-                key={user.id}
-                className={`border-b hover:bg-base-300 transition ${
-                  index % 2 === 0 ? "bg-base-200" : "bg-base-100"
-                }`}
-              >
-                <td className="p-4 font-medium text-base-content">{index + 1}</td>
-                <td className="p-4 font-semibold text-base-content">{user.name}</td>
-                <td className="p-4 font-semibold text-base-content">{user.username}</td>
-                <td className="p-4 font-medium text-base-content">{user.email}</td>
-                <td className="p-4 font-medium text-base-content">{user.role}</td>
-                <td className="p-4">
-                 <DeleteModal onDelete={handleDelete} deleteId={user.id} title="حذف کاربر 👤" message="آیا از حذف کاربر اطمینان دارید ؟" messageDesc="این اقدام قابل بازگشت نیست !" deleteBtnText="حذف کاربر"  />
-                </td>
-                <td className="p-4">
-                  <ChangeRoleModal user={user} />
-                </td>
-                <td className="p-4">
-                 <BanModal  onBan={handleBan} user={user}   />
-                </td>
-              </tr>
-            ))}
+            {isLoading
+              ?
+                [...Array(3)].map((_, index) => (
+                  <tr
+                    key={index}
+                    className="border-b bg-base-200 animate-pulse"
+                  >
+                    <td className="p-4">
+                      <div className="skeleton h-4 w-10 mx-auto"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-4 w-20 mx-auto"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-4 w-24 mx-auto"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-4 w-32 mx-auto"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-4 w-16 mx-auto"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-8 w-10 mx-auto rounded-md"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-8 w-10 mx-auto rounded-md"></div>
+                    </td>
+                    <td className="p-4">
+                      <div className="skeleton h-8 w-10 mx-auto rounded-md"></div>
+                    </td>
+                  </tr>
+                ))
+              : 
+                users.map((user, index) => (
+                  <tr
+                    key={user.id}
+                    className={`border-b hover:bg-base-300 transition ${
+                      index % 2 === 0 ? "bg-base-200" : "bg-base-100"
+                    }`}
+                  >
+                    <td className="p-4 font-medium text-base-content">
+                      {index + 1}
+                    </td>
+                    <td className="p-4 font-semibold text-base-content">
+                      {user.name}
+                    </td>
+                    <td className="p-4 font-semibold text-base-content">
+                      {user.username}
+                    </td>
+                    <td className="p-4 font-medium text-base-content">
+                      {user.email}
+                    </td>
+                    <td className="p-4 font-medium text-base-content">
+                      {user.role}
+                    </td>
+                    <td className="p-4">
+                      <DeleteModal
+                        onDelete={handleDelete}
+                        deleteId={user.id}
+                        title="حذف کاربر 👤"
+                        message="آیا از حذف کاربر اطمینان دارید ؟"
+                        messageDesc="این اقدام قابل بازگشت نیست !"
+                        deleteBtnText="حذف کاربر"
+                      />
+                    </td>
+                    <td className="p-4">
+                      <ChangeRoleModal user={user} />
+                    </td>
+                    <td className="p-4">
+                      <BanModal onBan={handleBan} user={user} />
+                    </td>
+                  </tr>
+                ))}
           </tbody>
         </table>
       </div>
