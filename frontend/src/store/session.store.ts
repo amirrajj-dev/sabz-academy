02642e9d1e49c4662e0dff04e8752cb1b243ebfe
amirrajj-dev <amiramraja@gmail.com>
2025-03-03 22:30:08 +0330

    add session logic and functioanllity completed🎓💚

--- frontend/src/store/session.store.ts
+++ frontend/src/store/session.store.ts
@@ -4,65 +4,101 @@ import axiosnInstance from "@/configs/axios";
 
 interface SessionStore {
   sessions: ISession[];
-  getAllSessions : (courseID : string)=>Promise<{message : string ; success : boolean}>;
+  getAllSessions: (
+    courseID: string
+  ) => Promise<{ message: string; success: boolean }>;
   setSession: (session: ISession[]) => void;
-  addSession: (session: ISession) => void;
-  deleteSession: (sessionId: string , courseId) => void; 
-  isLoading : boolean 
+  addSession: (
+    session: ISession
+  ) => Promise<{ message: string; success: boolean }>;
+  deleteSession: (
+    sessionId: string,
+    courseId: string
+  ) => Promise<{ message: string; success: boolean }>;
+  isLoading: boolean;
 }
 
-const useSessionStore = create<SessionStore>((set , get) => ({
-    sessions : [],
-    isLoading : false,
-    addSession(session) {
-        
-    },
-    deleteSession : async (sessionId , courseId) => {
-        try {
-          set({isLoading : true})
-          if (!sessionId || !courseId){
-            throw new Error('Invalid sessionId or courseId');
-          }
-          const res = await axiosnInstance.delete(`/courses/${courseId}/sessions/${sessionId}`)
-          if (res.data.success) {
-            set({ isLoading : false, sessions: get().sessions.filter((s) => s.id !== sessionId) });
-            return {
-              success: true,
-              message: res.data.message,
-            };
-          }else{
-            throw new Error(res.data.message)
-          }
-        } catch (error : any) {
-          return {
-            message: error.response.data.message || error.message,
-            success: false,
-          }
-        }
-    },  
-    setSession(session) {
-      set({ sessions: session });
-    },  
-    getAllSessions : async (courseID) => {
-      try {
-        if (!courseID){
-          throw new Error('Invalid courseID');
-        }
-        const res = await axiosnInstance.get(`/courses/${courseID}/sessions`)
-        if (res.data.success) {
-          set({sessions: res.data.data, isLoading : false });
-          return {
-            message: res.data.message,
-            success: true,
-          };
-        } else {
-          throw new Error(res.data.message || 'Failed to fetch sessions');
-        }
-      } catch (error : any) {
-        return {
-          message: error.response.data.message || error.message,
-          success: false,
-        }
+const useSessionStore = create<SessionStore>((set, get) => ({
+  sessions: [],
+  isLoading: false,
+  addSession: async (session) => {
+    try {
+      set({ isLoading: true });
+      const { title, time, courseId, video, free } = session;
+      if (!title || !time || !courseId || !video || !free) {
+        throw new Error("Please fill all fields");
       }
-    }, 
-}))
+      const res = await axiosnInstance.post(
+        `/courses/${courseId}/sessions`,
+        session
+      );
+      if (res.data.success) {
+        set({ sessions: [...get().sessions, res.data], isLoading: false });
+        return {
+          success: true,
+          message: res.data.message,
+        };
+      } else {
+        throw new Error(res.data.message);
+      }
+    } catch (error: any) {
+      return {
+        message: error.response.data.message || error.message,
+        success: false,
+      };
+    }
+  },
+  deleteSession: async (sessionId, courseId) => {
+    try {
+      set({ isLoading: true });
+      if (!sessionId || !courseId) {
+        throw new Error("Invalid sessionId or courseId");
+      }
+      const res = await axiosnInstance.delete(
+        `/courses/${courseId}/sessions/${sessionId}`
+      );
+      if (res.data.success) {
+        set({
+          isLoading: false,
+          sessions: get().sessions.filter((s) => s.id !== sessionId),
+        });
+        return {
+          success: true,
+          message: res.data.message,
+        };
+      } else {
+        throw new Error(res.data.message);
+      }
+    } catch (error: any) {
+      return {
+        message: error.response.data.message || error.message,
+        success: false,
+      };
+    }
+  },
+  setSession(session) {
+    set({ sessions: session });
+  },
+  getAllSessions: async (courseID) => {
+    try {
+      if (!courseID) {
+        throw new Error("Invalid courseID");
+      }
+      const res = await axiosnInstance.get(`/courses/${courseID}/sessions`);
+      if (res.data.success) {
+        set({ sessions: res.data.data, isLoading: false });
+        return {
+          message: res.data.message,
+          success: true,
+        };
+      } else {
+        throw new Error(res.data.message || "Failed to fetch sessions");
+      }
+    } catch (error: any) {
+      return {
+        message: error.response.data.message || error.message,
+        success: false,
+      };
+    }
+  },
+}));
