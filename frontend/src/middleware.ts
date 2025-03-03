0c758c621de0159c2577ab2f360699634e8038b9
amirrajj-dev <amiramraja@gmail.com>
2025-03-03 17:03:07 +0330

    route protection for signup , signin and admin pannel🎓💚

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import axiosnInstance from "./configs/axios";
+import { IUser } from "./interfaces/types";
+
+export async function middleware(request: NextRequest) {
+  console.log("middleware executed:", request.nextUrl.pathname);
+
+  const token = request.cookies.get("sabz-token")?.value;
+  console.log('Token:', token);
+
+  if (token) {
+    try {
+      const res = await axiosnInstance.get("/auth/me", {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        baseURL: "http://localhost:5000/api",
+      });
+      const user: IUser = res.data.user;
+      console.log('User:', user);
+
+      if (request.nextUrl.pathname.startsWith("/sign") && user.name) {
+        console.log("Redirecting to /");
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+
+      if (
+        request.nextUrl.pathname.startsWith("/admin") &&
+        user.role !== "ADMIN"
+      ) {
+        console.log("Redirecting to /");
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+    } catch (error) {
+      console.log('Error fetching user:', error);
+      return NextResponse.next();
+    }
+  }else{
+    if (request.nextUrl.pathname.startsWith("/admin")){
+        console.log("Redirecting to /");
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/signup", "/signin", "/admin-pannel"],
+};
