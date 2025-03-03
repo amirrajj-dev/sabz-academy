247029049660b00e54c635d8910d39d6771239fb
amirrajj-dev <amiramraja@gmail.com>
2025-03-03 22:16:03 +0330

    get all sessions logic and functioanllity completed🎓💚

--- frontend/src/store/session.store.ts
+++ frontend/src/store/session.store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { ISession } from "@/interfaces/types";
+import axiosnInstance from "@/configs/axios";
 
 interface SessionStore {
   sessions: ISession[];
@@ -22,7 +23,22 @@ const useSessionStore = create<SessionStore>((set) => ({
     setSession(session) {
         
     },  
-    getAllSessions(courseID) {
-      
+    getAllSessions : async (courseID) => {
+      try {
+        if (!courseID){
+          throw new Error('Invalid courseID');
+        }
+        const res = await axiosnInstance.get(`/courses/${courseID}/sessions`)
+        if (res.data.success) {
+          set({sessions: res.data.data, isLoading : false });
+        } else {
+          throw new Error(res.data.message || 'Failed to fetch sessions');
+        }
+      } catch (error : any) {
+        return {
+          message: error.response.data.message || error.message,
+          success: false,
+        }
+      }
     }, 
 }))
