0728121c1996e590bf9d9e3e1f3c9a72631f12c9
amirrajj-dev <amiramraja@gmail.com>
2025-03-04 21:16:33 +0330

    add article logic completed🎓💚

--- frontend/src/store/article.store.ts
+++ frontend/src/store/article.store.ts
@@ -17,8 +17,35 @@ export const useArticleStore = create<ArticlesStore>((set, get) => ({
     articles : [],
     mainArticle : null,
     isLoading : false,
-    addArticle(article) {
-        
+    addArticle : async (article) => {
+        try {
+            set({ isLoading : true })
+            const {body , categoryID , description , shortName , title , cover} = article
+            if (!body || !categoryID || !description || !shortName || !title || !cover){
+                throw new Error('all fields are required')
+            }
+            const formData = new FormData()
+            formData.append('title', title)
+            formData.append('description', description)
+            formData.append('body', body)
+            formData.append('file', cover)
+            formData.append('categoryID', categoryID)
+            formData.append('shortName', shortName)
+            const res = await axiosnInstance.post('/articles', formData, {
+                headers: { 'Content-Type':'multipart/form-data' },
+            })
+            if (res.data.success){
+                set({ articles: [...get().articles, res.data.data], isLoading : false })
+                return { message : res.data.message, success : true }
+            }else{
+                throw new Error(res.data.message || 'Failed to add article')
+            }
+        } catch (error : any) {
+            return {
+                message : error.response.data.message || error.message,
+                success : false
+            }
+        }
     },
     deleteArticle(id) {
         
