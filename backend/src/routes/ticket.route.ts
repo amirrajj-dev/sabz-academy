7867ba50087df84ddc862dbeb57d0e916acde952
amirrajj-dev <amiramraja@gmail.com>
2025-03-08 15:18:44 +0330

    added ticket route necessary end points🎓💚

--- backend/src/routes/ticket.route.ts
+++ backend/src/routes/ticket.route.ts
@@ -1,5 +1,11 @@
 import express from 'express'
+import { createTicket, getAllTickets, replyTicket } from '../controllers/ticket.controller';
+import { protectRoute, protectRouteAdmin } from '../middlewares/authMiddleware';
 
 const router = express.Router()
+
+router.get('/' , getAllTickets)
+router.post('/' , protectRoute as express.RequestHandler  , createTicket as express.RequestHandler)
+router.post('/:id' , protectRouteAdmin as express.RequestHandler  , replyTicket as express.RequestHandler)
 
 export default router;
