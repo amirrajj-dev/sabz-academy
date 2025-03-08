a4b2ce375011ec967a38ca7a596d70f93b04b44b
amirrajj-dev <amiramraja@gmail.com>
2025-03-08 15:38:23 +0330

    show tickets in admin pannel completed🎓💚

--- frontend/src/components/admin/ticket/TicketsPage.tsx
+++ frontend/src/components/admin/ticket/TicketsPage.tsx
@@ -1,43 +1,23 @@
-'use client'
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { FaEye, FaReply } from "react-icons/fa";
 import { motion } from "framer-motion";
 import DeleteModal from "@/components/modals/shared/DeleteModal";
+import { useTicketStore } from "@/store/ticket.store";
 
 const TicketsPage = () => {
-  const [tickets, setTickets] = useState([
-    {
-      id: 101,
-      user: "علی احمدی",
-      title: "مشکل ورود به حساب",
-      ticket: "من نمی‌توانم وارد حساب کاربری خود شوم.",
-      course: "دوره جاوا اسکریپت",
-      priority: "بالا",
-      status: "پاسخ داده شده",
-    },
-    {
-      id: 102,
-      user: "مریم رضایی",
-      title: "خطای پرداخت",
-      ticket: "پرداخت من انجام شده اما دسترسی ندارم.",
-      course: "دوره ری‌اکت",
-      priority: "متوسط",
-      status: "در انتظار پاسخ",
-    },
-    {
-      id: 103,
-      user: "رضا صالحی",
-      title: "سوال درباره پروژه",
-      ticket: "در بخش پروژه نهایی مشکل دارم.",
-      course: "دوره نود جی‌اس",
-      priority: "پایین",
-      status: "پاسخ داده شده",
-    },
-  ]);
+  const { tickets, fetchTickets, isLoading } = useTicketStore();
+  useEffect(() => {
+    fetchTickets();
+  }, []);
+
+  console.log(tickets);
 
   return (
     <div className="p-6 max-w-5xl mx-auto">
-      <h1 className="text-3xl font-extrabold text-base-content mb-4">🎫 تیکت ها</h1>
+      <h1 className="text-3xl font-extrabold text-base-content mb-4">
+        🎫 تیکت ها
+      </h1>
       <div className="shadow-lg rounded-xl overflow-x-auto">
         <table className="table bg-base-300 min-w-[800px] w-full text-center">
           <thead className="bg-base-300 text-base-content">
@@ -46,7 +26,6 @@ const TicketsPage = () => {
               <th className="p-4">کاربر</th>
               <th className="p-4">عنوان</th>
               <th className="p-4">تیکت</th>
-              <th className="p-4">دوره</th>
               <th className="p-4">الویت</th>
               <th className="p-4">مشاهده</th>
               <th className="p-4">پاسخ</th>
@@ -62,12 +41,25 @@ const TicketsPage = () => {
                   index % 2 === 0 ? "bg-base-200" : "bg-base-100"
                 }`}
               >
-                <td className="p-4 font-medium text-base-content">{ticket.id}</td>
-                <td className="p-4 font-semibold text-base-content">{ticket.user}</td>
-                <td className="p-4 font-semibold text-base-content">{ticket.title}</td>
-                <td className="p-4 text-base-content">{ticket.ticket}</td>
-                <td className="p-4 text-base-content">{ticket.course}</td>
-                <td className="p-4 text-base-content">{ticket.priority}</td>
+                <td className="p-4 font-medium text-base-content">
+                  {index + 1}
+                </td>
+                <td className="p-4 font-semibold text-base-content">
+                  {ticket.user.name}
+                </td>
+                <td className="p-4 font-semibold text-base-content">
+                  {ticket.title}
+                </td>
+                <td className="p-4 text-base-content">{ticket.title}</td>
+                <td className="p-4 text-base-content">
+                  {ticket.priority === "high" ? (
+                    <span className="size-5 inline-block bg-red-500 rounded-full"></span>
+                  ) : ticket.priority === "low" ? (
+                    <span className="size-5 inline-block bg-indigo-500 rounded-full"></span>
+                  ) : (
+                    <span className="size-5 inline-block bg-emerald-500 rounded-full"></span>
+                  )}
+                </td>
                 <td className="p-4">
                   <motion.button
                     whileHover={{ scale: 1.1 }}
@@ -84,7 +76,7 @@ const TicketsPage = () => {
                     <FaReply />
                   </motion.button>
                 </td>
-                <td className="p-4 text-base-content">{ticket.status}</td>
+                <td className="p-4 text-base-content">{ticket.status === 'open' ? 'پاسخ داده نشده' : "پاسخ داده شده"}</td>
               </tr>
             ))}
           </tbody>
