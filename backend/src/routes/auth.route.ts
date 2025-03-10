7ed0fb762ae70f24d0df8e068cb750fac05d1364
amirrajj-dev <amiramraja@gmail.com>
2025-03-10 22:29:15 +0330

    added necessary auth route for forget password🎓💚

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { signin, signout, signup , getMe } from '../controllers/auth.controller'
+import { signin, signout, signup , getMe , forgotPassword , resetPassword } from '../controllers/auth.controller'
 import { protectRoute } from '../middlewares/authMiddleware';
 
 const router = express.Router()
@@ -9,5 +9,8 @@ router.post('/signup', signup as express.RequestHandler);
 router.post('/signin' , signin as express.RequestHandler);
 router.get('/signout' , signout as express.RequestHandler);
 router.get('/me' , protectRoute as express.RequestHandler , getMe as express.RequestHandler as express.RequestHandler)
+
+router.post('/forgot-password', forgotPassword as express.RequestHandler);
+router.post('/reset-password/:token', resetPassword as express.RequestHandler);
 
 export default router;
