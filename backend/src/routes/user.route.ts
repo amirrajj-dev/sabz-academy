03003896cf01b6406ae2ee5cd1aeb16f5fb74780
amirrajj-dev <amiramraja@gmail.com>
2025-03-11 13:59:15 +0330

    pdated user route update user middlewares for upload profile🎓💚

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { protectRoute, protectRouteAdmin } from '../middlewares/authMiddleware';
 import { getAllUsers , banUser ,  changeUserRole , getUserCourses  , deleteUser , updateUser} from '../controllers/user.controller';
+import { upload } from '../../utils/fileUpload';
 
 const router = express.Router()
 
 router.get('/' , protectRouteAdmin as express.RequestHandler ,  getAllUsers as express.RequestHandler)
-router.put('/' , protectRoute as express.RequestHandler , updateUser as express.RequestHandler)
+router.put('/' , protectRoute as express.RequestHandler , upload , updateUser as express.RequestHandler)
 router.delete('/:id' , protectRouteAdmin as express.RequestHandler , deleteUser as express.RequestHandler)
 router.put('/ban/:id' , protectRouteAdmin as express.RequestHandler , banUser as express.RequestHandler)
 router.get('/courses' , protectRoute as express.RequestHandler , getUserCourses as express.RequestHandler)
