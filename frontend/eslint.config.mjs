3a7102e7f082d9845d84c7f662cfc8d07b4e98c7
amirrajj-dev <amiramraja@gmail.com>
2025-03-15 00:24:17 +0330

    add a more compatiable axios  verison🎓💚

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -12,8 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    "@typescript-eslint/no-explicit-any" : "off"
-  }
+    rules: {
+      "@typescript-eslint/no-explicit-any": "any",
+    },
+  },
 ];
 
 export default eslintConfig;
